2765b1d913eddf8c35d57ec0be11caf6e36373c5
Aleksey Zheleznyakov <135155537+AlekseyZheleznyakov@users.noreply.github.com>
2023-06-15 15:02:26 +0300

    Add files via upload
    Add mouse control

--- Mandelbrot/main.js
+++ Mandelbrot/main.js
@@ -8,7 +8,17 @@ function loadShader(gl, type, source) {
     let buf = gl.getShaderInfoLog(shader);
     console.log(buf);
   }
+
   return shader;
+}
+
+function checkCheckbox(id) {
+  const checkbox = document.getElementById(id);
+  if (checkbox.checked) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function initGL() {
@@ -30,6 +40,10 @@ function initGL() {
       out highp vec4 o_color;
 
       uniform highp float time;
+      uniform highp float result;
+      uniform highp float dx;
+      uniform highp float dy;
+      uniform highp float zoom;
 
       highp vec2 CmplSet( highp float a, highp float b )
       {
@@ -99,17 +113,20 @@ function initGL() {
       }
 
       void main() {
-        highp float n = 0.0, x0 = -2.0, x1 = 2.0, y0 = -2.0, y1 = 2.0;
+        highp float n = 0.0, x0 = -zoom, y0 = -zoom, x1 = zoom, y1 = zoom;
         highp float a, b;
         highp vec2 Z;
 
-        a = x0 + gl_FragCoord.x * (x1 - x0) / 700.0;
-        b = y0 + gl_FragCoord.y * (y1 - y0) / 700.0;
-        Z = CmplSet(a, b);
-        n = Julia(Z);
-        
-        //  Z = CmplSet(x0 + gl_FragCoord.x * (x1 - x0) / 700.0, y0 + gl_FragCoord.y * (y1 - y0) / 700.0);
-        //  n = Mandelbrot(Z);
+        if (result == 1.0) {
+          a = x0 + (gl_FragCoord.x - dx) * (x1 - x0) / 700.0;
+          b = y0 + (gl_FragCoord.y - dy) * (y1 - y0) / 700.0;
+          Z = CmplSet(a, b);
+          n = Julia(Z);  
+        }
+        if (result == 0.0) {
+          Z = CmplSet(x0 + (gl_FragCoord.x - dx) * (x1 - x0) / 700.0, y0 + (gl_FragCoord.y - dy) * (y1 - y0) / 700.0);
+          n = Mandelbrot(Z);
+        }
 
         o_color = vec4(n / 255.0, n / 8.0 / 255.0, n * 8.0 / 255.0, 1);
       }
@@ -123,6 +140,7 @@ function initGL() {
   gl.attachShader(program, vertexSh);
   gl.attachShader(program, fragmentSh);
   gl.linkProgram(program);
+  let result = 0.0;
 
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
       alert("!!!!");
@@ -131,15 +149,66 @@ function initGL() {
                -1, -1, 0, 1,
                1, 1, 0, 1,
                1, -1, 0, 1];
-                     
+
+  window.mouse = {
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0
+  };   
+  window.center = {
+    x: 0,
+    y: 0
+  }
+  window.zoom = 1.0;
 
   const render = () => {
+    window.addEventListener("mousedown", (event) => {
+      if (event.clientX < 800 && event.clientY > 120) {
+          window.isHold = true;
+      }
+    });
+    window.addEventListener("mouseup", (event) => {
+        if (true || event.clientX < 800 && event.clientY > 120) {
+            window.isHold = false;
+        }
+    });
+    window.addEventListener("mousemove", (event) => {
+        if (window.isHold) {
+            window.mouse.dx += (event.movementX / 300.0);
+            window.mouse.dy -= (event.movementY / 300.0);
+
+            window.center.x += event.movementX * window.zoom / 300.0;
+            window.center.y += event.movementY * window.zoom / 300.0;
+        }
+    });
+    window.addEventListener("wheel", (event) => {
+      window.zoom += event.deltaY / 500000.0 * window.zoom;
+
+      window.mouse.dx = window.center.x / window.zoom;
+      window.mouse.dy = window.center.y / window.zoom;
+    });
+
+    if (checkCheckbox("chooseMandl")) {
+        result = 0.0;
+    }
+    if (checkCheckbox("chooseJul")) {
+        result = 1.0;
+    }
+
     const posLoc = gl.getAttribLocation(program, "in_pos");
     const posBuf = gl.createBuffer();
-
     const timeFromStart = Date.now() - start;
     const loc = gl.getUniformLocation(program, "time");
     gl.uniform1f(loc, timeFromStart / 1000.0);
+    const loc1 = gl.getUniformLocation(program, "result");
+    gl.uniform1f(loc1, result);
+    const loc2 = gl.getUniformLocation(program, "dx");
+    gl.uniform1f(loc2, window.mouse.dx);
+    const loc3 = gl.getUniformLocation(program, "dy");
+    gl.uniform1f(loc3, window.mouse.dy);
+    const loc4 = gl.getUniformLocation(program, "zoom");
+    gl.uniform1f(loc4, window.zoom);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuf);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pos), gl.STATIC_DRAW);
